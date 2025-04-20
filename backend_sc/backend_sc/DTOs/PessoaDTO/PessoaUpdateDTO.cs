ef1712fff5847747699f98cc712158455fc5c533
WellingtonDOF <116592362+WellingtonDOF@users.noreply.github.com>
2025-04-20 04:17:51 -0300

    Terminado backend de aluno (#6)

---
+++ backend_sc/backend_sc/DTOs/PessoaDTO/PessoaUpdateDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend_sc.DTOs.PessoaDTO
+{
+    public class PessoaUpdateDTO
+    {
+        [MaxLength(150)]
+        public string NomeCompleto { get; set; }
+
+        [MaxLength(150)]
+        public string Endereco { get; set; }
+
+        [Phone]
+        [MaxLength(22)]
+        public string Telefone { get; set; }
+
+        [EmailAddress]
+        [MaxLength(150)]
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+    }
+}
--- backend_sc/backend_sc/Services/PessoaService/IPessoaInterface.cs
+++ backend_sc/backend_sc/Services/PessoaService/IPessoaInterface.cs
@@ -1,12 +1,13 @@
 ﻿using backend_sc.DTOs.PessoaDTO;
 using backend_sc.Models;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace backend_sc.Services.PessoaService
 {
     public interface IPessoaInterface
     {
         Task<ServiceResponse<List<PessoaResponseDTO>>> GetPessoas();
-        Task<ServiceResponse<PessoaResponseDTO>> CreatePessoa(PessoaCreateDTO newPessoa);
+        Task<ServiceResponse<PessoaResponseDTO>> CreatePessoa(PessoaCreateDTO newPessoal);
         Task<ServiceResponse<PessoaResponseDTO>> GetPessoaById(int id);
         Task<ServiceResponse<PessoaResponseDTO>> UpdatePessoa(PessoaModel editPessoa);
         Task<ServiceResponse<bool>> DeletePessoa(int id);
